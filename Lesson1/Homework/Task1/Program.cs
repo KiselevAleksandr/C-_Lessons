49c650fa867154a42027066604d47737bed7ee14
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-03 00:35:28 +0300

    added task1

---
+++ Lesson1/Homework/Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите число a: " );
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число b: " );
+int b = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (a > b)
+{
+    max = a;
+    Console.WriteLine ("Максимальное число = "+max);
+}
+else
+{
+    max = b;
+    Console.WriteLine ("Максимальное число = "+max);
+}
