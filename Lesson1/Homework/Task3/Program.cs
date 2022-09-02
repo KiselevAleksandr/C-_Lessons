6eb76f093f42ce34aa2999a98e7daf3cd3bbaae9
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-03 01:59:47 +0300

    added task3

---
+++ Lesson1/Homework/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write ("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 1)
+{
+    Console.WriteLine("Число " + x + " является: Нечетным");
+}
+else
+{
+    Console.WriteLine("Число " + x + " является: Четным");
+}
