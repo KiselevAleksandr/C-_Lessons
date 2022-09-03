2ca882d39409ccbfc9abc6c66ce2685b02794e6e
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-03 21:55:28 +0300

    added task1

--- Lesson2/Homework/Task1/Program.cs
+++ Lesson2/Homework/Task1/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+while (number > 100)
+{
+    number /= 10;
+    result = number % 10;
+}
+Console.Write("Вторая цифра числа: " + result);
