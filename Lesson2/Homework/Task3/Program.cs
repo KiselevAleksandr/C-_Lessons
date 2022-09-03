d11fb655d4196baf479cf80e5f602fdf7e8fe9b3
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-03 22:01:38 +0300

    added task3

---
+++ Lesson2/Homework/Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Напишите цифру , обозначающую день недели (от 1 до 7): ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 5)
+{
+    Console.WriteLine("Это рабочий день! ");
+}
+else
+{
+    Console.WriteLine("Это выходной! ");
+}
