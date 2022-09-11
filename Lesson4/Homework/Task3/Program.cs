1f7988333a83716c803d41fcf9d1bf39766fce94
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-11 15:32:25 +0300

    added lesson4

---
+++ Lesson4/Homework/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите массив чисел через пробел: ");
+    string input = Console.ReadLine();
+    string[] elements = input.Split(" ");
+    int[] array = new int[elements.Length];
+
+    for (int i = 0; i < elements.Length; i++)
+    {
+        array[i] = Convert.ToInt32(elements[i]);
+    }
+
+    Console.WriteLine("");
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[i] + ", ");
+    }
+
+    Console.WriteLine(array[array.Length - 1] + "]");
+
