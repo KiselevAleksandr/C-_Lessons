9a5d1a67925b5bde4f767f230ff8daf554e9dc10
KiselevAleksandr <kiselevgit@bk.ru>
2022-09-26 17:53:06 +0300

    added lesson7

---
+++ Lesson7/Homework/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите размер массива m*n ");
+int m = int.Parse(Console.ReadLine());
+int n = int.Parse(Console.ReadLine());
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) / 10;
+        array[i, j] = Math.Round(new Random().NextDouble(), 2);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
---
+++ Lesson7/Homework/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[,] numbers = new int[3, 3];
+double[] average = new double[numbers.GetLength(1)];
+
+Console.WriteLine("Задана матрица: ");
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        numbers[i, j] = new Random().Next(0, 10);
+        Console.Write(numbers[i, j] + " ");
+        average[j] += Convert.ToDouble(numbers[i, j]) / numbers.GetLength(0);
+    }
+    Console.WriteLine("");
+}
+Console.WriteLine("Среднее арифметическое столбца: ");
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    Console.Write(Math.Round(average[j], 1) + " ");
+}
